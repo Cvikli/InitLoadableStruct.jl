390e38ef7fa5a6115608cf2a7851e479df55081e
Marcell <havlikmarcell@gmail.com>
2024-02-25 23:18:11 +0100

    @interface functions

--- src/InitLoadableStruct.jl
+++ src/InitLoadableStruct.jl
@@ -1,18 +1,20 @@
 module InitLoadableStruct
 
+using Unimplemented
+
 export InitableLoadable
 
 abstract type InitableLoadable end
 
 # To make this work, you have to have the "default" constructor for your type without the "data" part (so having empty data part)
-init(t::Type{TYPE}, args...; kw_args...) where TYPE = TYPE(args...; kw_args...)
+init(t::Type{TYPE}, args...; kw_args...)      where TYPE = TYPE(args...; kw_args...)
 
 
 # When the object is inited/configured, this is the function that have to able to download the right data. So you have to overload this as your data query expect!
 load_data(t::Type{TYPE}, args...; kw_args...) where TYPE = load_data!(init(t, args...; kw_args...))
-load_data!(obj::TYPE)                    where TYPE = throw("Implement load_data(...)")
+@interface load_data!(obj::TYPE)              where TYPE 
 
-unique_args(obj::TYPE)                   where TYPE = throw("We need the fields that specify the data for caching")
+@interface unique_args(obj::TYPE)             where TYPE
 
 
 end # module InitLoadableStruct
